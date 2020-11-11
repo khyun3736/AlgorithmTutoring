8f795fb2999a32992d87bb4e9b81dcb48ab6990e
khyun3736 <66210403+khyun3736@users.noreply.github.com>
2020-11-11 21:12:05 +0900

    Add files via upload

---
+++ 리모컨.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+
+count = 0
+while a != b:
+    count += 1
+    if abs(a-b) >= 8:
+        if a > b:
+            a -= 10
+        else:
+            a += 10
+    elif abs(a-b) >= 3:
+        if a > b:
+            a -= 5
+        else:
+            a += 5
+    else:
+        if a > b:
+            a -= 1
+        else:
+            a += 1
+    
+print(count)
